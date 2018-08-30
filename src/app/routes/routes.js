3fe8f18f6173f1f4e93b93c1190322202d25a917
carloshjoaquim <carloshj2011@gmail.com>
2018-08-30 06:22:23 -0300

    Refatorando acesso a dados

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -7,7 +7,7 @@ module.exports = function(app){
 
     app.get('/produtos',function(request, response){
         var connection = app.infra.ConnectionFactory();
-        var produtosBanco = new app.infra.produtosBanco(connection);
+        var produtosBanco = new app.infra.ProdutosDAO(connection);
          
        produtosBanco.lista(function(error, result){
             response.render('produtos/lista', {lista:result});
@@ -18,7 +18,7 @@ module.exports = function(app){
 
     app.get('/produtos/remove',function(){
         var connection = app.infra.ConnectionFactory();
-        var produtosBanco = new app.infra.produtosBanco(connection);
+        var produtosBanco = new app.infra.ProdutosDAO(connection);
          
         var produto = produtosBanco.carrega(id, callback);
         if(produto)
